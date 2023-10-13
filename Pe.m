74823c2e1c2e980206575d44c70e53b494e6b202
amatov <6277757+amatov@users.noreply.github.com>
2023-10-13 10:22:03 -0700

    Add files via upload

---
+++ Pe.m
@@ -0,0 +1,11 @@
+
+figure(1);
+[a]=textread('home/matov/matlab/Pe_X2.txt','%f');
+[b]=textread('home/matov/matlab/Pe_Y2.txt','%f');
+plot(a(16:20),b(16:20));
+xlabel('S/N, dB');
+ylabel('log P(E)');
+title('Error probability');
+
+set(gcf, 'DefaultTextFontSize', 16);
+set(gca, 'FontSize', 16) ;
